d18aacbc9bc8753c340a2b245adc13247e2a3d04
amine <aelqaraoui101@gmail.com>
2022-03-01 14:48:55 +0100

    W

--- index.js
+++ index.js
@@ -42,6 +42,9 @@ app.get("/api/all_transactions", async (req, res) => {
 
 const get_player_info = (transactions, address) => {
 	let result = 0
+	let volume = 0
+	let streak = 0
+	let do_streaks = true
 	for(let i = 0; i < transactions.length; i++)
 	{
 		
@@ -53,13 +56,25 @@ const get_player_info = (transactions, address) => {
 			}
 			//console.log(transactions[i].outcome, amount)
 			result += transactions[i].outcome ? 1 : 0 * amount
+			volume += amount
+
+			if(do_streaks)
+			{
+				if(transactions[i].outcome)
+				{
+					streak += 1
+				}
+				else{
+					do_streaks = false
+				}
+			}
 		}
 		
 	}
-	return {'signer_id': address, 'net': result}
+	return {'signer_id': address, 'net': result, 'volume': volume, 'streak': streak}
 }
 
-app.get("/api/leaderboard", async (req, res) => {
+app.get("/api/leaderboard/net-gain", async (req, res) => {
 	const response = await fetch(`https://indexer.havendao.community/api/house.woothugg.near?api_key=6ad95b1bc0c3c4f2901c73d2&limit=999999`);
 
 
@@ -96,6 +111,102 @@ app.get("/api/leaderboard", async (req, res) => {
 	let leaderboard = player_info.map((value) => {return {'signer_id': value.signer_id, 'net': value.net}});
 	leaderboard.sort(function(a, b) {
 		return b.net - a.net;
+	});
+
+	return res.json({
+		'total_flips': transactions.length,
+		'total_won': total_won,
+		'total_loss': total_loss,
+		'total_volume': total_volume,
+		'leaderboard': leaderboard.slice(0, 20)
+	})
+})
+
+app.get("/api/leaderboard/volume", async (req, res) => {
+	const response = await fetch(`https://indexer.havendao.community/api/house.woothugg.near?api_key=6ad95b1bc0c3c4f2901c73d2&limit=999999`);
+
+
+	const results = await response.json();
+
+	const transactions = results.data.filter((value) => value.signer_id != 'house.woothugg.near');
+
+	console.log(transactions)
+
+	let total_volume = 0;
+	let total_won = 0;
+	let total_loss = 0;
+	for(let i = 0; i < transactions.length; i++)
+	{
+		let amount = parseInt(transactions[i].amount)
+		if(amount > 6) {
+			amount /= 1035000000000000000000000
+		}
+
+		total_volume += amount;
+		if(transactions[i].outcome)
+		{
+			total_won += amount;
+		}
+		else
+		{
+			total_loss += amount;
+		}
+	}
+
+	let unique_players = [...new Set(transactions.map((value) => value.signer_id))];
+	let player_info = unique_players.map((value) => get_player_info(transactions, value));
+
+	let leaderboard = player_info.map((value) => {return {'signer_id': value.signer_id, 'volume': value.volume}});
+	leaderboard.sort(function(a, b) {
+		return b.volume - a.volume;
+	});
+
+	return res.json({
+		'total_flips': transactions.length,
+		'total_won': total_won,
+		'total_loss': total_loss,
+		'total_volume': total_volume,
+		'leaderboard': leaderboard.slice(0, 20)
+	})
+})
+
+app.get("/api/leaderboard/streak", async (req, res) => {
+	const response = await fetch(`https://indexer.havendao.community/api/house.woothugg.near?api_key=6ad95b1bc0c3c4f2901c73d2&limit=999999`);
+
+
+	const results = await response.json();
+
+	const transactions = results.data.filter((value) => value.signer_id != 'house.woothugg.near');
+
+	console.log(transactions)
+
+	let total_volume = 0;
+	let total_won = 0;
+	let total_loss = 0;
+	for(let i = 0; i < transactions.length; i++)
+	{
+		let amount = parseInt(transactions[i].amount)
+		if(amount > 6) {
+			amount /= 1035000000000000000000000
+		}
+
+		total_volume += amount;
+		if(transactions[i].outcome)
+		{
+			total_won += amount;
+		}
+		else
+		{
+			total_loss += amount;
+		}
+	}
+
+	let unique_players = [...new Set(transactions.map((value) => value.signer_id))];
+	let player_info = unique_players.map((value) => get_player_info(transactions, value));
+
+	let leaderboard = player_info.map((value) => {return {'signer_id': value.signer_id, 'streak': value.streak}});
+	leaderboard.sort(function(a, b) {
+		return b.streak - a.streak;
 	});
 
 	return res.json({
